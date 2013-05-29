5186187d6463b2ffb52665aca8ea74d05255b9d5
cambell <cambell.prince@gmail.com>
2013-05-29 22:04:25 +0700

    route command: refactor layer_producer command handling and add channel_producer support.  e.g. LOADBG 2-1 route://1

--- protocol/amcp/AMCPCommandsImpl.h
+++ protocol/amcp/AMCPCommandsImpl.h
@@ -25,7 +25,13 @@
 
 #include "AMCPCommand.h"
 
-namespace caspar { namespace protocol {
+namespace caspar {
+
+namespace core {
+	struct frame_producer;
+}
+
+namespace protocol {
 	
 std::wstring ListMedia();
 std::wstring ListTemplates();
@@ -78,6 +84,8 @@ class RouteCommand : public AMCPCommandBase<true, AddToQueue, 1>
 {
 	std::wstring print() const { return L"RouteCommand";}
 	bool DoExecute();
+public:
+	static safe_ptr<core::frame_producer> TryCreateProducer(AMCPCommand& command, std::wstring const& uri);
 };
 
 class LoadCommand : public AMCPCommandBase<true, AddToQueue, 1>
