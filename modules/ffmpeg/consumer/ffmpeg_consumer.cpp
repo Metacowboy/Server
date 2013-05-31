229b16bb37ccf11ea5438e4e775726512f1d28d8
cambell <cambell.prince@gmail.com>
2013-05-31 17:23:08 +0700

    ffmpeg_consumer: refactoring the default options for stream

--- modules/ffmpeg/consumer/ffmpeg_consumer.cpp
+++ modules/ffmpeg/consumer/ffmpeg_consumer.cpp
@@ -649,57 +649,55 @@ public:
 	}
 };	
 
+struct FFMPEG_Option
+{
+	char const* key;
+	char const* value;
+};
+
 safe_ptr<core::frame_consumer> create_consumer(const core::parameters& params)
 {
 	if(params.size() < 1 || params[0] != L"FILE")
 		return core::frame_consumer::empty();
 	
+	bool use_default_options = params.size() < 3;
+
 	auto filename	= (params.size() > 1 ? params[1] : L"");
 			
 	// Add UDP or RTP Stream output
-	bool bStream = false;
+	bool is_stream = false;
 	if( filename.substr(0,4) == L"UDP:" )
 	{
 		boost::replace_all(filename, L"UDP", L"udp");
 		CASPAR_LOG(info) << L"Stream URL: "+filename;
-		bStream = true;
+		is_stream = true;
 	}
 	else
 	if( filename.substr(0,4) == L"RTP:" )
 	{
 		boost::replace_all(filename, L"RTP", L"rtp");
-		bStream = true;
+		is_stream = true;
 	}
 
 	std::vector<option> options;
-	
-	// Default stream output format MPEGTS MPEG4 AAC
-	if( bStream && params.size() < 3 )
+	if( is_stream && use_default_options )
 	{
+		// Default stream output format MPEGTS MPEG4 AAC
 		// -f mpegts -vcodec mpeg4 -acodec mp2 -ab 128000 -s 640x360 -b 1000000
-		auto name  = narrow(L"f");
-		auto value = narrow(L"mpegts");
-		options.push_back(option(name, value));
-		name  = narrow(L"vcodec");
-		value = narrow(L"mpeg4");
-		options.push_back(option(name, value));
-		name  = narrow(L"acodec");
-		value = narrow(L"aac");
-		options.push_back(option(name, value));
-		name  = narrow(L"ab");
-		value = narrow(L"128000");
-		options.push_back(option(name, value));
-		name  = narrow(L"s");
-		//value = narrow(L"720x405");
-		value = narrow(L"640x360");
-		options.push_back(option(name, value));
-		name  = narrow(L"b");
-		//value = narrow(L"1200000");
-		value = narrow(L"1000000");
-		options.push_back(option(name, value));
+		static FFMPEG_Option defaults[] = {
+			{ "f",      "mpegts"  },
+			{ "vcodec", "mpeg4"   },
+			{ "acodec", "aac"     },
+			{ "ab",     "128000"  },
+			{ "s",      "640x360" },
+			{ "b",      "1000000" }
+		};
+		for (int i = 0; i < sizeof(defaults) / sizeof(FFMPEG_Option); ++i) {
+			options.push_back(option(defaults[i].key, defaults[i].value));
+		}
 	}
 
-	if(params.size() >= 3)
+	if(!use_default_options)
 	{
 		for(auto opt_it = params.begin()+2; opt_it != params.end();)
 		{
@@ -715,7 +713,7 @@ safe_ptr<core::frame_consumer> create_consumer(const core::parameters& params)
 		}
 	}
 		
-	if( bStream )
+	if( is_stream )
 		return make_safe<ffmpeg_consumer_proxy>(filename, options);
 	else
 		return make_safe<ffmpeg_consumer_proxy>(env::media_folder() + filename, options);
